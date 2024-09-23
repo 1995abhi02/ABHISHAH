6ba15d7f5659fa31c09e7d942501c585c1afeaec
1995abhi02 <abhishah.data@gmail.com>
2024-09-23 17:35:42 +0530

    Update remote.py

--- remote.py
+++ remote.py
@@ -1,1 +1,11 @@
 This is addded by another developer.
+
+  this is added remotrly ...line 2
+  this is added remotrly ...line 3
+
+
+
+
+
+this is added remotrly ...line 10
+this is added remotrly ...line 11
